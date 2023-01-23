44809a44724a32ea21b7789d10af4e593b22307d
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-01-23 07:31:13 +0200

    Create Help.ts

---
+++ src/Commands/General/Help.ts
@@ -0,0 +1,58 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+import { IArgs } from '../../Types'
+
+@Command('help', {
+    description: "Displays the bot's usable commands",
+    aliases: ['h', 'menu', 'commands'],
+    cooldown: 10,
+    exp: 20,
+    usage: 'help || help <command_name>',
+    category: 'general'
+})
+export default class extends BaseCommand {
+    public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
+        if (!context) {
+            let commands = Array.from(this.handler.commands, ([command, data]) => ({
+                command,
+                data
+            })).filter((command) => command.data.config.category !== 'dev')
+            const image = this.client.assets.get('hitman') as Buffer
+            const { nsfw } = await this.client.DB.getGroup(M.from)
+            if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
+            const buffer = await this.client.utils.getBuffer('https://telegra.ph/file/f477a71cf52d751d48068.mp4')
+            let text = `hitman✨! *@${M.sender.jid.split('@')[0]}*, I'm ${
+                this.client.config.name
+            }\nMy prefix is - "${this.client.config.prefix}"\n\n  1. *Don't Call* bots to avoid blocking \n\n  3. *The usable commands* are listed below.`
+            const categories: string[] = []
+            for (const command of commands) {
+                if (categories.includes(command.data.config.category)) continue
+                categories.push(command.data.config.category)
+            }
+            for (const category of categories) {
+                const categoryCommands: string[] = []
+                const filteredCommands = commands.filter((command) => command.data.config.category === category)
+                text += `\n\n*╚━☾︎🌹 ${this.client.utils.capitalize(category)} 🌹☽︎━╝*\n\n`
+                filteredCommands.forEach((command) => categoryCommands.push(command.data.name))
+                text += `\`\`\`${categoryCommands.join(', ')}\`\`\``
+            }
+            text += `\n\n📕 *Note:* Use ${this.client.config.prefix}help <command_name> for more info of a specific command\n\n😻 Example: *${this.client.config.prefix}help 𝕙𝕖𝕝𝕝𝕠*\n\n♧︎︎︎𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌 = 𝐊𝐎𝐘𝐄𝐁*`
+            return void (await M.reply(buffer, 'video', true, undefined, text, [M.sender.jid]))
+        } else {
+            const cmd = context.trim().toLowerCase()
+            const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
+            if (!command) return void M.reply(`No command found | *"${context.trim()}"*`)
+            return void M.reply(
+                `🎐 *Command:* ${this.client.utils.capitalize(command.name)}\n🎴 *Aliases:* ${
+                    !command.config.aliases
+                        ? ''
+                        : command.config.aliases.map((alias) => this.client.utils.capitalize(alias)).join(', ')
+                }\n🔗 *Category:* ${this.client.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
+                    command.config.cooldown ?? 3
+                }s\n🎗 *Usage:* ${command.config.usage
+                    .split('||')
+                    .map((usage) => `${this.client.config.prefix}${usage.trim()}`)
+                    .join(' | ')}\n🧧 *Description:* ${command.config.description}`
+            )
+        }
+    }
+}
